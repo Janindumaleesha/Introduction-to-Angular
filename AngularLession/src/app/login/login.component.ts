533e7a181c505481b7dc5e752850fd3f5faeea58
Janindumaleesha <janindu.maleesha@app360.lk>
2025-08-07 00:50:30 +0530

    Completed lession 04

---
+++ AngularLession/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core"; 
+
+@Component ({
+    selector: 'app-login',
+    standalone: true,
+    // template: `<h1>Login Component</h1> 
+    // <p>I m 26 year old</p>`,
+    templateUrl: 'login.component.html',
+    styleUrl: 'login.component.css'
+})
+
+export class LoginComponent {
+
+}
